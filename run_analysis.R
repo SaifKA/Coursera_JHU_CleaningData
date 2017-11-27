2c0508a44205ca29426065ebe93d934956dc3d96
SaifKA <33718578+SaifKA@users.noreply.github.com>
2017-11-27 13:06:56 +0600

    Intermediate Codebook

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,11 @@
+getwd()
+setwd("D://Shiny//Projects//3. Clean/")
 
+if (!dir.exists("data")){
+  dir.create("data")
+}
+
+dir()
 
 ####################   Project   ###################################
 
@@ -74,7 +81,6 @@ str(train_subjects)
 train_1<-cbind(train,train_labels,train_subjects)
 names(train_1)
 
-### Added Descriptive Variable Names - Instruction#4
 
 names(train_1)<-c(features$V2,'label','subject')
 
@@ -91,10 +97,10 @@ mean_std_Data<-mergeData[,grepl('mean()|std()|label|subject',names(mergeData)) ]
 str(mean_std_Data)
 names(mean_std_Data)
 
-# Exclude meanFreq
-mean_std_Data<-mean_std_Data[,!grepl('meanFreq',names(mean_std_Data) )]
-str(mean_std_Data)
-names(mean_std_Data)
+# # Exclude meanFreq
+# mean_std_Data<-mean_std_Data[,!grepl('meanFreq',names(mean_std_Data) )]
+# str(mean_std_Data)
+# names(mean_std_Data)
 
 ### Descriptive Activity Names - Instruction#3
 
@@ -104,11 +110,104 @@ Final_raw_data<-mean_std_Data%>%
   select(-label)
 
 
+### Added Descriptive Variable Names - Instruction#4
+
+names(Final_raw_data)<-c(
+  "time_domain_body_acceleration_mean_X_Axis"	,
+  "time_domain_body_acceleration_mean_Y_Axis"	,
+  "time_domain_body_acceleration_mean_Z_Axis"	,
+  "time_domain_body_acceleration_standard_deviation_X_Axis"	,
+  "time_domain_body_acceleration_standard_deviation_Y_Axis"	,
+  "time_domain_body_acceleration_standard_deviation_Z_Axis"	,
+  "time_domain_gravity_acceleration_mean_X_Axis"	,
+  "time_domain_gravity_acceleration_mean_Y_Axis"	,
+  "time_domain_gravity_acceleration_mean_Z_Axis"	,
+  "time_domain_gravity_acceleration_standard_deviation_X_Axis"	,
+  "time_domain_gravity_acceleration_standard_deviation_Y_Axis"	,
+  "time_domain_gravity_acceleration_standard_deviation_Z_Axis"	,
+  "time_domain_body_acceleration_jerk_mean_X_Axis"	,
+  "time_domain_body_acceleration_jerk_mean_Y_Axis"	,
+  "time_domain_body_acceleration_jerk_mean_Z_Axis"	,
+  "time_domain_body_acceleration_jerk_standard_deviation_X_Axis"	,
+  "time_domain_body_acceleration_jerk_standard_deviation_Y_Axis"	,
+  "time_domain_body_acceleration_jerk_standard_deviation_Z_Axis"	,
+  "time_domain_body_gyroscope_reading_mean_X_Axis"	,
+  "time_domain_body_gyroscope_reading_mean_Y_Axis"	,
+  "time_domain_body_gyroscope_reading_mean_Z_Axis"	,
+  "time_domain_body_gyroscope_reading_standard_deviation_X_Axis"	,
+  "time_domain_body_gyroscope_reading_standard_deviation_Y_Axis"	,
+  "time_domain_body_gyroscope_reading_standard_deviation_Z_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_mean_X_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_mean_Y_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_mean_Z_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_standard_deviation_X_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_standard_deviation_Y_Axis"	,
+  "time_domain_body_gyroscope_jerk_reading_standard_deviation_Z_Axis"	,
+  "time_domain_body_acceleration_magnitude_mean"	,
+  "time_domain_body_acceleration_magnitude_standard_deviation"	,
+  "time_domain_gravity_acceleration_magnitude_mean"	,
+  "time_domain_gravity_acceleration_magnitude_standard_deviation"	,
+  "time_domain_body_gyroscope_jerk_magnitudereading_mean"	,
+  "time_domain_body_gyroscope_jerk_magnitudereading_standard_deviation"	,
+  "time_domain_body_gyroscope_magnitude_reading_mean"	,
+  "time_domain_body_gyroscope_magnitude_reading_standard_deviation"	,
+  "time_domain_body_gyroscope_jerk_magnitude_reading_mean"	,
+  "time_domain_body_gyroscope_jerk_magnitude_reading_standard_deviation"	,
+  "frequency_domain_body_acceleration_mean_X_Axis"	,
+  "frequency_domain_body_acceleration_mean_Y_Axis"	,
+  "frequency_domain_body_acceleration_mean_Z_Axis"	,
+  "frequency_domain_body_acceleration_standard_deviation_X_Axis"	,
+  "frequency_domain_body_acceleration_standard_deviation_Y_Axis"	,
+  "frequency_domain_body_acceleration_standard_deviation_Z_Axis"	,
+  "frequency_domain_body_acceleration_mean_frequency_X_Axis"	,
+  "frequency_domain_body_acceleration_mean_frequency_Y_Axis"	,
+  "frequency_domain_body_acceleration_mean_frequency_Z_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_X_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_Y_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_Z_Axis"	,
+  "frequency_domain_body_acceleration_jerk_standard_deviation_X_Axis"	,
+  "frequency_domain_body_acceleration_jerk_standard_deviation_Y_Axis"	,
+  "frequency_domain_body_acceleration_jerk_standard_deviation_Z_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_frequency_X_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_frequency_Y_Axis"	,
+  "frequency_domain_body_acceleration_jerk_mean_frequency_Z_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_X_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_Y_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_Z_Axis"	,
+  "frequency_domain_body_gyroscope_reading_standard_deviation_X_Axis"	,
+  "frequency_domain_body_gyroscope_reading_standard_deviation_Y_Axis"	,
+  "frequency_domain_body_gyroscope_reading_standard_deviation_Z_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_frequency_X_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_frequency_Y_Axis"	,
+  "frequency_domain_body_gyroscope_reading_mean_frequency_Z_Axis"	,
+  "frequency_domain_body_acceleration_magnitude_mean"	,
+  "frequency_domain_body_acceleration_magnitude_standard_deviation"	,
+  "frequency_domain_body_acceleration_magnitude_mean_frequency"	,
+  "frequency_domain_body_gyroscope_jerk_magnitudereading_mean"	,
+  "frequency_domain_body_gyroscope_jerk_magnitudereading_standard_deviation"	,
+  "frequency_domain_body_gyroscope_jerk_magnitudereading_mean_frequency"	,
+  "frequency_domain_body_gyroscope_magnitude_reading_mean"	,
+  "frequency_domain_body_gyroscope_magnitude_reading_standard_deviation"	,
+  "frequency_domain_body_gyroscope_magnitude_reading_mean_frequency"	,
+  "frequency_domain_body_gyroscope_jerk_magnitude_reading_mean"	,
+  "frequency_domain_body_gyroscope_jerk_magnitude_reading_standard_deviation"	,
+  "frequency_domain_body_gyroscope_jerk_magnitude_reading_mean_frequency"	,
+  "subject"	,
+  "activity"	
+)
+
 ### Tidy Dataset - Instruction#5
 
 Final_tidy_data<-Final_raw_data%>%
+  mutate(
+    activity=as.factor(activity),
+    subject=as.factor(subject)
+  )%>%
   group_by(activity,subject)%>%
   summarise_all(mean)
 
+str(Final_tidy_data)
+
 write.table(Final_tidy_data,file="Final_tidy_data.txt",row.names = FALSE)
+
 
